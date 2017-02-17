c2033c2713b0a2bddc94dece805c1fc285da9bcc
zhangxuchao <2212362272@qq.com>
2017-02-17 17:12:14 +0800

    Add files via upload

---
+++ components/blog.component.ts
@@ -0,0 +1,8 @@
+import {Component} from "@angular/core";
+@Component({
+    selector: "blog-ng2",
+    templateUrl: "../templates/blog.tpl.html"
+})
+export class BlogComponent {
+
+}
---
+++ services/page.service.ts
@@ -0,0 +1,12 @@
+import {Injectable} from "@angular/core";
+@Injectable()
+export class PageService {
+    page: number;
+    constructor() {
+        this.page = 1;
+    }
+    changePage(currentPage) {
+        this.page = currentPage;
+        return currentPage;
+    }
+}
